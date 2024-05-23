8c0ba7bef8c3dfbcad270ed84a7c4e712874cf72
José Vitor dos Santos Fernandes <jose-vitor-7@hotmail.com>
2024-05-23 19:49:22 -0300

    build: fix prettier requirements

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,31 +9,31 @@ const Home: NextPage = () => {
         <nav className="mx-auto mt-[5.375rem] flex max-w-[1200px] flex-col items-center justify-between gap-4 md:flex-row">
           <Image src={logo} alt="Jera Movies logo" className="h-10" />
           <div className="flex gap-4">
-            <button className="bg-brand-primary text-brand-secondary-500 border-brand-primary-900 hover:border-brand-primary-900 rounded-lg border-[1px] px-6 py-2 transition-all md:border-transparent">
+            <button className="bg-brand-primary rounded-lg border-[1px] border-brand-primary-900 px-6 py-2 text-brand-secondary-500 transition-all hover:border-brand-primary-900 md:border-transparent">
               SIGN UP
             </button>
-            <button className="bg-brand-accent-500 text-brand-primary-500 rounded-lg px-6 py-2">
+            <button className="rounded-lg bg-brand-accent-500 px-6 py-2 text-brand-primary-500">
               SIGN IN
             </button>
           </div>
         </nav>
 
         <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-          <h1 className="text-brand-accent-500 font-anton text-9xl md:text-[24vw] lg:text-[18rem]">
+          <h1 className="font-anton text-9xl text-brand-accent-500 md:text-[24vw] lg:text-[18rem]">
             JERA
           </h1>
         </div>
       </section>
-      <section className="bg-brand-secondary-500 h-screen">
+      <section className="h-screen bg-brand-secondary-500">
         {/* Movies */}
       </section>
-      <section className="bg-brand-secondary-500 h-screen">
+      <section className="h-screen bg-brand-secondary-500">
         {/* With */}
       </section>
-      <section className="bg-brand-accent-500 h-screen">
+      <section className="h-screen bg-brand-accent-500">
         {/* Profiles */}
       </section>
-      <section className="bg-brand-secondary-500 h-screen">
+      <section className="h-screen bg-brand-secondary-500">
         {/* Footer */}
       </section>
     </div>
