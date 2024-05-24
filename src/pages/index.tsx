9e0193d68978d9d64e55a859218787f9c4e4da80
José Vitor dos Santos Fernandes <jose-vitor-7@hotmail.com>
2024-05-23 23:39:59 -0300

    fix: form alignment for sign up and sign-in

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,36 @@
 import { NextPage } from 'next'
 import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 import logo from 'public/logo.svg'
+import { useCallback } from 'react'
 
 const Home: NextPage = () => {
+  const router = useRouter()
+
+  const handleSignIn = useCallback(() => {
+    router.push('/sign-in')
+  }, [router])
+
+  const handleSignUp = useCallback(() => {
+    router.push('/sign-up')
+  }, [router])
+
   return (
     <div>
       <section className="bg-brand-primary h-screen">
         <nav className="mx-auto flex max-w-[1200px] flex-col items-center justify-between gap-4 pt-[5.375rem] md:flex-row">
           <Image src={logo} alt="Jera Movies logo" className="h-10" />
           <div className="flex gap-4">
-            <button className="bg-brand-primary rounded-lg border border-brand-primary-900 px-6 py-2 text-brand-secondary-500 transition-all hover:border-brand-primary-900 md:border-transparent">
+            <button
+              className="bg-brand-primary rounded-lg border border-brand-primary-900 px-6 py-2 text-brand-secondary-500 transition-all hover:border-brand-primary-900 md:border-transparent"
+              onClick={handleSignIn}
+            >
               SIGN UP
             </button>
-            <button className="rounded-lg bg-brand-accent-500 px-6 py-2 text-brand-primary-500">
+            <button
+              className="rounded-lg bg-brand-accent-500 px-6 py-2 text-brand-primary-500"
+              onClick={handleSignUp}
+            >
               SIGN IN
             </button>
           </div>
